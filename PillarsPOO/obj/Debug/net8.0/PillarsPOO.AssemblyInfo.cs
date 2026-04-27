6cd5e5090dab1764820b929152b80ad3bcc8182f
Ismael Quiroz Santes <ismaelqzs@gmail.com>
2026-04-26 22:58:22 -0600

    updates

--- PillarsPOO/obj/Debug/net8.0/PillarsPOO.AssemblyInfo.cs
+++ PillarsPOO/obj/Debug/net8.0/PillarsPOO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PillarsPOO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e18d16012e0f62bfadd4947da231af3bdc0b959b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7bb52d091724ba734bef334d265ba696f1f7bc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PillarsPOO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PillarsPOO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
